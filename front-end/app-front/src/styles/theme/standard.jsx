b99a5b19d47fd3e1876ea2d4c389195cab58d370
Yure7S <yuresilvaprof.com>
2023-03-14 17:24:42 -0300

    chore: refinando estrutura do diretório

---
+++ front-end/app-front/src/styles/theme/standard.jsx
@@ -0,0 +1,34 @@
+export const standard = {
+   colors: {
+     primary: '#0070f3',
+     secondary: '#616161',
+     success: '#28a745',
+     danger: '#dc3545',
+     warning: '#ffc107',
+     info: '#17a2b8',
+     light: '#f8f9fa',
+     dark: '#343a40',
+   },
+   typography: {
+     fontFamily: 'Roboto, sans-serif',
+     fontSize: {
+       xs: '12px',
+       sm: '14px',
+       md: '16px',
+       lg: '20px',
+       xl: '24px',
+     },
+     fontWeight: {
+       light: 300,
+       regular: 400,
+       bold: 700,
+     },
+   },
+   spacing: {
+     xs: '4px',
+     sm: '8px',
+     md: '16px',
+     lg: '24px',
+     xl: '32px',
+   },
+ };
